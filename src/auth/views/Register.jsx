6fb8362f0f48754024c505255377abd407c94fa3
Daniel Perez Gonzalez <isc.pgd@gmail.com>
2025-02-07 20:56:38 -0600

    Feat: vista de registro

--- src/auth/views/Register.jsx
+++ src/auth/views/Register.jsx
@@ -1,5 +1,44 @@
+import { Button, Grid2, TextField, Typography, Link } from "@mui/material"
+import { AuthLayout } from "../layout/AuthLayout"
+import { Google } from "@mui/icons-material"
+import { Link as RouterLink } from "react-router"
+
 export const Register = () => {
     return (
-        <div>Register</div>
+        <AuthLayout title="Crear una cuenta">
+            <form>
+                <Grid2 container spacing={2}>
+                    <TextField
+                        label="Nombre completo"
+                        type="text"
+                        placeholder="Octavio Roca de Leija"
+                        sx={{ mb: 1 }}
+                        fullWidth
+                    />
+                    <TextField
+                        label="Correo"
+                        type="email"
+                        placeholder="correo@google.com"
+                        sx={{ mb: 1 }}
+                        fullWidth
+                    />
+                    <TextField
+                        label="Contraseña"
+                        type="password"
+                        sx={{ mb: 2 }}
+                        fullWidth
+                    />
+                    <Button sx={{mb:2}} variant="contained" fullWidth>
+                        <Typography textTransform="capitalize">Crear cuenta</Typography>
+                    </Button>
+                </Grid2>
+            </form>
+            <Grid2 container justifyContent="end">
+                <Typography sx={{mr: 1}}>¿Ya tienes una cuenta?</Typography>
+                <Link align="right" component={RouterLink} color="primary" to="/auth/login">
+                    Iniciar sesión
+                </Link>
+            </Grid2>
+        </AuthLayout>
     )
 }
