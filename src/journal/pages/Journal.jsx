0daf74323463777fd5e7d50fb818685da58ef503
Daniel Perez Gonzalez <isc.pgd@gmail.com>
2025-02-08 10:59:38 -0600

    Feat: boton flotante

--- src/journal/pages/Journal.jsx
+++ src/journal/pages/Journal.jsx
@@ -1,15 +1,24 @@
-import { Typography } from "@mui/material"
+import { Box, Fab, IconButton, Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
 import { Note, NothingSelected } from "../views"
+import { Add } from "@mui/icons-material"
 
 export const Journal = () => {
     return (
         <JournalLayout>
             {/* Nothing selected */}
-            {/* <NothingSelected /> */}
+            <NothingSelected />
 
             {/* Note view */}
-            <Note />
+            {/* <Note /> */}
+
+            {/* Floating button */}
+            <Box sx={{ position: 'fixed', right: 30, bottom: 30}}>
+                <Fab color="primary" variant="extended" aria-label="add">
+                    <Add sx={{mr: 1}} />
+                    Agregar nueva nota
+                </Fab>
+            </Box>
         </JournalLayout>
     )
 }
