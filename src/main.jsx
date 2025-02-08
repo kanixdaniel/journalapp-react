d2852edb0e5a66162e1b3f5d2929423c7ca67e51
Daniel Perez Gonzalez <isc.pgd@gmail.com>
2025-02-08 17:31:40 -0600

    Feat: configuracion base de redux

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import { JournalApp } from './JournalApp'
 import './styles.css'
 import { AppTheme } from './theme'
+import { Provider } from 'react-redux'
+import { store } from './redux/store'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <AppTheme>
-            <JournalApp />
-        </AppTheme>
+        <Provider store={store}>
+            <AppTheme>
+                <JournalApp />
+            </AppTheme>
+        </Provider>
     </StrictMode>,
 )
